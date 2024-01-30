9b901d7a8b664bc4471f1a4904a941d89a97f5da
Eduardo Soares <eduardo.soares@hp.com>
2024-01-30 12:41:18 -0300

    Revert apt package installation

--- main.sh
+++ main.sh
@@ -3,11 +3,6 @@ source scripts/helpers/toolset-scripts.sh
 # Create workspace folder
 # cd && mkdir workspace && cd wsl_setup
 
-# Execute installer scripts
-for installer in scripts/installers/*.sh; do
-	bash "$installer"
-done
-
 # Install APT packages from toolset file
 echo "Running apt update"
 sudo apt update
@@ -16,6 +11,11 @@ packages=$(get_toolset_value .apt_packages[])
 for package in $packages; do
 	echo "Installing $package"
 	sudo apt install $package
+done
+
+# Execute installer scripts
+for installer in scripts/installers/*.sh; do
+	bash "$installer"
 done
 
 # Append aliases inside .bashrc file
