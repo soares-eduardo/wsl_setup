b12967a393c7522ebf8d4ddadc1a8dc45dc01f4f
Eduardo Soares <eduardo.soares@hp.com>
2024-01-30 12:01:51 -0300

    Add script to install kubectl

---
+++ scripts/installers/kubectl.sh
@@ -0,0 +1,9 @@
+source $HOME/wsl_setup/scripts/helpers/toolset-scripts.sh
+
+version=$(get_toolset_value .kubectl.version)
+
+curl -LO "https://dl.k8s.io/release/$version/bin/linux/amd64/kubectl"
+
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+rm kubectl
