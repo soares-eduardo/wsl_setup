bf3678a02e714cf85942680a3bee4d7990ee0551
Eduardo Soares <eduardo.soares@hp.com>
2024-01-30 12:03:21 -0300

    Update main script by changing installation order

--- main.sh
+++ main.sh
@@ -3,6 +3,11 @@ source scripts/helpers/toolset-scripts.sh
 # Create workspace folder
 # cd && mkdir workspace && cd wsl_setup
 
+# Execute installer scripts
+for installer in scripts/installers/*.sh; do
+	bash "$installer"
+done
+
 # Install APT packages from toolset file
 echo "Running apt update"
 sudo apt update
@@ -11,11 +16,6 @@ packages=$(get_toolset_value .apt_packages[])
 for package in $packages; do
 	echo "Installing $package"
 	sudo apt install $package
-done
-
-# Execute installer scripts
-for installer in scripts/installers/*.sh; do
-	bash "$installer"
 done
 
 # Append aliases inside .bashrc file
