b8b6c434590fce58236f48c6b1aab6f468cab14d
soares-eduardo <eduardoandre.mcc@gmail.com>
2024-02-22 20:56:27 -0300

    Refactor main file with bats package

--- main.sh
+++ main.sh
@@ -3,11 +3,13 @@ source scripts/helpers/toolset-scripts.sh
 # Create workspace folder
 # cd && mkdir workspace && cd wsl_setup
 
-# Install APT packages from toolset file
 echo "Running apt update"
 sudo apt update
-sudo apt install -y jq
 
+# Install mandatory packages
+sudo apt install -y jq bats
+
+# Install APT packages from toolset file
 packages=$(get_toolset_value .apt_packages[])
 for package in $packages; do
 	echo "Installing $package"
